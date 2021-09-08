363858bf788b4d860060da936f7a4422d882e5a0
Hivemindless <giogeta87@gmail.com>
2021-09-08 16:19:32 +0200

    Update build.groovy

--- libraries/node/steps/build.groovy
+++ libraries/node/steps/build.groovy
@@ -1,7 +1,15 @@
 void call(){
         stage("Node: Build"){
                 println "build from the node library"
-                sh "npm --version"
+                node {
+                        env.NODEJS_HOME = "${tool 'Node 6.x'}"
+                        // on linux / mac
+                        env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+                        // on windows
+                        env.PATH="${env.NODEJS_HOME};${env.PATH}"
+                        sh 'npm --version'
+                }
+                //sh "npm --version"
                 //sh "npm install"
                 //sh("npm run start")
         }
