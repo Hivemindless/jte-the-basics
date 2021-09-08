21ab66c188c88c58e615d05798a5167fc9991f93
Hivemindless <giogeta87@gmail.com>
2021-09-08 16:04:32 +0200

    Update build.groovy

--- libraries/node/steps/build.groovy
+++ libraries/node/steps/build.groovy
@@ -1,7 +1,7 @@
 void call(){
         stage("Node: Build"){
                 println "build from the node library"
-                sh "npm install"
+                //sh "npm install"
                 //sh("npm run start")
         }
 }
