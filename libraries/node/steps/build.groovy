05362109279fa11bbb3168afdac5432c9cccc600
Hivemindless <giogeta87@gmail.com>
2021-09-09 11:20:17 +0200

    Update build.groovy

--- libraries/node/steps/build.groovy
+++ libraries/node/steps/build.groovy
@@ -1,13 +1,3 @@
-def call(){
-        stage("Node: Build"){
-                println "build from the node library"
-                steps {
-                       // nodejs(nodeJSInstallationName: 'Node 6.x', configId: '<config-file-provider-id>') {
-                                sh 'npm --version'
-                        //}
-                }
-                
-                //sh "npm install"
-                //sh("npm run start")
-        }
+docker.image("node").inside{
+    sh "npm -v"
 }
