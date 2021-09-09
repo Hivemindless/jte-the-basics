099af482d19c49f2a175c8dc159396f126c00c8d
Hivemindless <giogeta87@gmail.com>
2021-09-09 10:36:34 +0200

    Update build.groovy

--- libraries/node/steps/build.groovy
+++ libraries/node/steps/build.groovy
@@ -2,11 +2,11 @@ def call(){
         stage("Node: Build"){
                 println "build from the node library"
                 steps {
-                        nodejs(nodeJSInstallationName: 'Node 6.x', configId: '<config-file-provider-id>') {
-                                sh 'npm config ls'
-                        }
+                       // nodejs(nodeJSInstallationName: 'Node 6.x', configId: '<config-file-provider-id>') {
+                                sh 'npm --version'
+                        //}
                 }
-                //sh "npm --version"
+                
                 //sh "npm install"
                 //sh("npm run start")
         }
